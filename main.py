ebcb9babe8bb2fd8c39b12c7092634605c70dfc5
gskishan004 <gskishan004@users.noreply.github.com>
2018-01-05 14:29:52 +0530

    Update main.py
    squashed some bugs

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from .nlp import extract_required_entities
-from .vision import detect_text
+from nlp import extract_required_entities
+from vision import detect_text
 
-access_token = ""
+access_token_NLP = "AIzaSyD68bu29Fz6o5SA1XgmKKnEFLsT2GANQew"
+access_token_VIS = "AIzaSyDjPaFGT8u9INRU9r7JA2a7_syWkNdAPzU"
 
 def extract_entities_from_img(img_path):
 
-    text = detect_text(img_path, access_token)
-    entities = extract_required_entities(text, access_token)
+    text = detect_text(img_path, access_token_NLP)
+    #entities = extract_required_entities(text, access_token_VIS)
 
-    return entities
+    return text
+	
+print (extract_entities_from_img("DL.jpg"))
